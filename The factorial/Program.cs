4def6d9913b03b0040129c4ba164e4f2e4c22d10
Mohammed Yaba <91971316+Gardi95@users.noreply.github.com>
2022-07-04 11:55:31 +0200

    first commit

---
+++ The factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace The_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, f = 1, i;
+
+
+
+            Console.WriteLine("Please Enter the Valeu:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+            {
+                Console.WriteLine("the number is pozative");
+
+                for (i = 1; i <= num; i++)
+                {
+                    f = f * i;
+                    Console.WriteLine(f);
+
+
+                }
+                Console.WriteLine($"The Factorial of {num} is: {f}");
+            }
+            else
+            {
+                Console.WriteLine("The number Nigtive");
+            }
+        }
+    }
+}
